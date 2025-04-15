f9d3acca8e8a1c9254884469b3eb5617375e9d8a
damonc <damonc@dtu.dk>
2025-04-15 15:01:13 +0200

    IG UI improvements

---
+++ qcodesplusplus/plotting/offline/convertdesign_hplappy.bat
@@ -0,0 +1,4 @@
+call C:/Users/damonc/AppData/Local/anaconda3/Scripts/activate.bat
+call conda activate pyqtsandbox
+call cd C:/git/qcodesplusplus/qcodesplusplus/plotting/offline
+call pyuic5 -x design.ui -o design.py
