b394e2ad45901735bd23223dfeab8c373bb66f00
djcarrad <djcarrad@gmail.com>
2025-09-17 10:27:22 +0200

    Make export 2D to IG

--- qcodespp/__init__.py
+++ qcodespp/__init__.py
@@ -64,7 +64,7 @@ from qcodespp.actions import Task, Wait, BreakIf
 
 from qcodespp.plotting.RemotePlot import Plot, live_plot
 from qcodespp.plotting.offline.main import offline_plotting
-from qcodespp.plotting.analysis_tools import colorplot, colored_traces, load_2d_json
+from qcodespp.plotting.analysis_tools import colorplot, colored_traces, load_2d_json, export_2d_to_IG
 
 from qcodespp.data.data_set import new_data, load_data, load_data_num, load_data_nums, set_data_format, set_data_folder
 
