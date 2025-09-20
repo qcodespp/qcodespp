e14a69d0c0f4a38a2553710941cd68c12132db22
djcarrad <djcarrad@gmail.com>
2025-09-20 13:33:08 +0200

    Measure class works

--- qcodespp/measure.py
+++ qcodespp/measure.py
@@ -4,12 +4,13 @@ import numpy as np
 from qcodes.utils import full_class, thread_map
 from qcodes.metadatable import Metadatable
 
-from qcodespp.parameters import Parameter
+from qcodespp.parameters import Parameter, MultiParameter
 from qcodespp.actions import _actions_snapshot
 from qcodespp.station import Station
 from qcodespp.data.data_set import new_data
 from qcodespp.data.data_array import DataArray
 from qcodespp.actions import _QcodesBreak
+from qcodespp.plotting.RemotePlot import live_plot
 
 
 class Measure(Metadatable):
@@ -22,7 +23,7 @@ class Measure(Metadatable):
     At init, you can provide the parameters to measure and (optionally) setpoints. If no parameters are 
     provided, the default station.measure() is used.
     If no setpoints are provided, dummy setpoints are created for each dimension
-    found in the parametersn (recommended, see below).
+    found in the measured parameters (recommended, see below).
 
     ``Measure.run()`` will execute the measurement, and return and save a ``DataSetPP``
 
@@ -49,14 +50,10 @@ class Measure(Metadatable):
         >>>     data=measure.run(name=f'iteration {i}')
 
     Args:
-        parameters (Optional, Sequence[Parameter]): Sequence of gettable Parameters.
+        *measure (Optional, Sequence[Parameter]): Sequence of gettable Parameters.
             If no actions are provided, the default station.measure() is used.
 
         name (Optional, str): String to send to the filename of the DataSet.
-
-        station (Optional, Station): The ``Station`` to use if not the default.
-
-        use_threads (Optional, bool): Use threading to parallelize getting parameters from instruments.
 
         setpoints (Optional, Sequence[Parameter or Array]): sequence of setpoint arrays
             use for the DataSetPP. Can be array(s) of values, or gettable Parameter(s). 
@@ -66,20 +63,24 @@ class Measure(Metadatable):
             and offline_plotting will then be able to plot the correct dependencies automatically. However; 
             it can be tricky to get all the dimensions right, so in most instances it's better to just pass 
             all measured parameters to the parameters argument, and then plot whatever parameter against 
-            whatever other parameter manually. 
+            whatever other parameter manually.
+
+        station (Optional, Station): The ``Station`` to use if not the default.
+
+        use_threads (Optional, bool): Use threading to parallelize getting parameters from instruments.
     
         timer (Optional, bool, default False): The default station.measure() includes a timer parameter, which is
             useful for Loops but essentially useless here. If you really want it, set timer=True.
     """
 
-    def __init__(self, setpoints=None, parameters=None,use_threads=False,station=None,name=None, timer=False):
+    def __init__(self, *measure, setpoints=None, use_threads=False, station=None, name=None, timer=False):
         super().__init__()
         self.station = station or Station.default
         self.use_threads = use_threads
         self.setpoints = setpoints
-        self.name=name
-        self.timer=timer
-        self.actions=parameters
+        self.name = name
+        self.timer = timer
+        self.actions = measure
 
     def each(self, *actions):
         """
@@ -103,13 +104,145 @@ class Measure(Metadatable):
         """
         return self.run(quiet=True, location=False, **kwargs)
     
-    def _containers(self):
+    def run(self, plot=None,name=None,use_threads=None, quiet=False, station=None,**kwargs):
         """
-        Makes the DataArrays to hold the setpoints and measured parameters.
+        Run the actions in this measurement and return their data as a DataSetPP
+
+        Args:
+            plot: a list of parameters to plot. Provide the parameter, 
+                or parameter full name as a string.
+
+            name (Optional[str]): Filename, minus counter, date and time.
+                Overwrites any name provided at init.
+
+            quiet (Optional[bool]): Set True to not print anything except
+                errors. Default False.
+
+            station (Optional[Station]): the ``Station`` this measurement
+                pertains to. Defaults to ``Station.default`` if one is defined.
+                Only used to supply metadata.
+
+            use_threads (Optional[bool]): whether to parallelize ``get``
+                operations using threads. Default False.
+
+            kwargs are passed to data_set.new_data. The key ones are:
+
+            location (Optional[Union[str, False]]): the location of the
+                DataSetPP, a string whose meaning depends on formatter and io,
+                or False to only keep in memory. May be a callable to provide
+                automatic locations. If omitted, will use the default
+                DataSetPP.location_provider
+
+            formatter (Optional[Formatter]): For writing to file. Default 
+                is GnuplotFormat, can be set in DataSetPP.default_formatter
+
+            io (Optional[io_manager]): io manager for DataSetPP object.
+
+        returns:
+            a DataSetPP object containing the results of the measurement
         """
+        if name:
+            self.name = name
+        station = station or self.station or Station.default
+
+        if self.timer==True:
+            station.timer.reset_clock()
+
+        if use_threads is not None:
+            self.use_threads = use_threads
+
+        try:
+            self._raw_data=self._measure()
+                
+        except _QcodesBreak:
+            self.was_broken=True
+
+        data_set = self._make_data_set(**kwargs)
+
+        if plot is not None:
+            pp=live_plot()
+            data_set.publisher = pp
+            pp.add_subplots(*plot)
+
+        if station:
+            data_set.add_metadata({'station': station.snapshot()})
+
+        data_set.add_metadata({'measurement': self.snapshot()})
+        ts = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        data_set.add_metadata({'measurement': {
+            'ts': ts,
+            'use_threads': use_threads,
+        }})
+
+        data_set.save_metadata()
+
+        data_set.finalize()
+
+        if not quiet:
+            print(repr(data_set))
+            print(datetime.now().strftime('Acquired at %Y-%m-%d %H:%M:%S'))
+
+        return data_set
+    
+    def _construct_measure_dict(self):
+        """
+        Construct a dict with all the parameters to measure, and whether they are setpoints or not.
+        """
+        params_to_measure={}
+        if not self.actions:
+            self.actions = self.station.measure()
+        if self.timer==False: # Useful in the Loop, so is included in station.measure() by default. Less useful here.
+            self.actions = [action for action in self.actions if action.name != 'timer']
+        if self.setpoints:
+            for action in self.setpoints:
+                if hasattr(action, '_gettable') and action._gettable: # don't try to measure a parameter without a get method.
+                    params_to_measure[action.full_name] = {'action': action, 'is_setpoint': True}
+        for action in self.actions:
+            if hasattr(action, '_gettable') and action._gettable: # don't try to measure a parameter without a get method.
+                params_to_measure[action.full_name] = {'action': action, 'is_setpoint': False}
+        return params_to_measure
+    
+    def _measure(self):
+        """
+        Perform the measurement: get all the parameters and store in a dict which will be used to fill a DataSetPP.
+        """
+        # TODO: At the moment there is none of the optimisations that (allegedly) exist in Loop,
+        # such as trying to group gettable parameters that have the same source.
+        out_dict = {}
+        params_to_measure=self._construct_measure_dict()
+
+        getters = [param['action'].get for param in params_to_measure.values()]
+
+        if self.use_threads: # Not tested
+            out = thread_map(getters)
+        else:
+            out = [g() for g in getters]
+
+        for param_out, param in zip(out, params_to_measure):
+            action=params_to_measure[param]['action']
+            if isinstance(action, MultiParameter):
+                for i, name in enumerate(action.names):
+                    out_dict[name] = {'values':param_out[i],
+                                    'unit':action.units[i],
+                                    'is_setpoint': params_to_measure[param]['is_setpoint']}
+            else:
+                out_dict[param] = {'values':param_out,
+                                            'unit':action.unit,
+                                            'is_setpoint': params_to_measure[param]['is_setpoint']}
+
+        return out_dict
+
+    def _make_data_set(self,**kwargs):
+        """
+        Construct the DataSetPP for this measurement.
+        
+        In contrast to Loop, this should not be called directly, but only
+        when the user calls Measure.run()
+        """
+
+        self.data_set=new_data(name=self.name,**kwargs)
+
         setpoint_arrays=[]
-        param_arrays=[]
-        arrays=[]
 
         if self.setpoints:
             for i,sp in enumerate(self.setpoints):
@@ -121,17 +254,17 @@ class Measure(Metadatable):
                                             is_setpoint=True,
                                             preset_data=sp)
                 elif isinstance(sp, Parameter):
-                    if hasattr(sp, 'shape'):
-                        shape = sp.shape
-                    else:
-                        shape = np.shape(sp.get_latest())
-                    setpoint_array = DataArray(parameter=sp, shape=shape, is_setpoint=True)
+                    vals=self._raw_data[sp.full_name]['values']
+                    setpoint_array = DataArray(parameter=sp, preset_data=vals, is_setpoint=True)
                 else:
                     raise TypeError("Setpoints element must be a Parameter or an array-like object")
                 setpoint_array.init_data()
                 setpoint_arrays.append(setpoint_array)
+                self.data_set.add_array(setpoint_array)
             
         for name,value in self._raw_data.items():
+            if value['is_setpoint']:
+                continue # Setpoints are already added above
             array_shape=np.shape(value['values'])
             if array_shape==(): # if shape is (), make it (1,) to make a setpoint array also.
                 array_shape=(1,)
@@ -160,148 +293,15 @@ class Measure(Metadatable):
                                             preset_data=dummy_setpoints)
                     setpoint_array.init_data()
                     setpoint_arrays.append(setpoint_array)
+                    self.data_set.add_array(setpoint_array)
 
                 param_setpoint_arrays=param_setpoint_arrays + (setpoint_array,)
-            data_array=DataArray(name=name, full_name=name, unit=value['unit'], preset_data=value['values'],
+            data_array=DataArray(name=name, unit=value['unit'], preset_data=value['values'],
                                 is_setpoint=False, set_arrays=param_setpoint_arrays)
-            param_arrays.append(data_array)
-        arrays = setpoint_arrays + param_arrays
+            data_array.init_data()
+            self.data_set.add_array(data_array)
 
-        # A potential TODO here: I feel like that in general the setpoints and measurement arrays will be created in the 'right' order,
-        # but maybe there are cases when this doesn't happen? If we run into such unintuitive cases, we could sort the arrays.
-        # Eg at the moment it's ALL setpoints, then ALL measured params, but maybe it's more intuative to group the setpoints and measured params.
-        return arrays
-
-    def _make_data_set(self):
-        """
-        Construct the DataSetPP for this measurement.
-        
-        In contrast to Loop, this should not be called directly, but only
-        when the user calls Measure.run()
-        """
-        self.data_set=new_data(name=self.name)
-        for array in self._containers():
-            if isinstance(array, DataArray):
-                self.data_set.add_array(array)
-            else:
-                raise TypeError(f"Expected DataArray, got {type(array)}")
         return self.data_set
-
-    def run(self, name=None,params_to_plot=None,use_threads=None, quiet=False, station=None, 
-            publisher=None,**kwargs):
-        """
-        Run the actions in this measurement and return their data as a DataSetPP
-
-        Args:
-            params_to_plot: a list of parameters to plot once the measurement is done.
-                Can either be the DataArray objects, or the parameters themselves.
-            quiet (Optional[bool]): Set True to not print anything except
-                errors. Default False.
-
-            station (Optional[Station]): the ``Station`` this measurement
-                pertains to. Defaults to ``Station.default`` if one is defined.
-                Only used to supply metadata.
-
-            use_threads (Optional[bool]): whether to parallelize ``get``
-                operations using threads. Default False.
-
-            Other kwargs are passed along to data_set.new_data. The key ones
-            are:
-
-            location (Optional[Union[str, False]]): the location of the
-                DataSetPP, a string whose meaning depends on formatter and io,
-                or False to only keep in memory. May be a callable to provide
-                automatic locations. If omitted, will use the default
-                DataSetPP.location_provider
-
-            name (Optional[str]): if location is default or another provider
-                function, name is a string to add to location to make it more
-                readable/meaningful to users
-
-            formatter (Optional[Formatter]): knows how to read and write the
-                file format. Default can be set in DataSetPP.default_formatter
-
-            io (Optional[io_manager]): knows how to connect to the storage
-                (disk vs cloud etc)
-
-        returns:
-            a DataSetPP object containing the results of the measurement
-        """
-        if name:
-            self.name = name
-        station = station or self.station or Station.default
-
-        if self.timer==True:
-            station.timer.reset_clock()
-
-        if use_threads is not None:
-            self.use_threads = use_threads
-
-        try:
-            self._raw_data=self._measure()
-                
-        except _QcodesBreak:
-            self.was_broken=True
-
-        data_set = self._make_data_set()
-
-        if publisher:
-            data_set.publisher = publisher
-
-        if station:
-            data_set.add_metadata({'station': station.snapshot()})
-
-        data_set.add_metadata({'measurement': self.snapshot()})
-        ts = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        data_set.add_metadata({'measurement': {
-            'ts': ts,
-            'use_threads': use_threads,
-        }})
-
-        data_set.save_metadata()
-
-        data_set.finalize()
-
-        if not quiet:
-            print(repr(data_set))
-            print(datetime.now().strftime('Acquired at %Y-%m-%d %H:%M:%S'))
-
-        return data_set
-    
-    def _measure(self):
-        """
-        Perform the measurement: get all the parameters and store in a dict which will be used to fill a DataSetPP.
-        """
-        # TODO: At the moment there is none of the optimisations that (allegedly) exist in Loop,
-        # such as trying to group gettable parameters that have the same source.
-        out_dict = {}
-        params_to_measure=[]
-        if not self.actions:
-            self.actions = self.station.measure()
-        if self.timer==False: # Useful in the Loop, so is included in station.measure() by default, but is completely useless here.
-            self.actions = [action for action in self.actions if action.name != 'timer']
-        for action in self.actions:
-            if hasattr(action, '_gettable') and action._gettable: # don't try to measure a parameter without a get method.
-                params_to_measure.append(action)
-
-        #param_ids, getters = zip(*((param.full_name, param.get) for param in params_to_measure))
-        getters = [param.get for param in params_to_measure]
-
-        if self.use_threads: # Not tested
-            out = thread_map(getters)
-        else:
-            out = [g() for g in getters]
-
-        for param_out, param in zip(out, params_to_measure):
-            if hasattr(param, 'names'): #MultiParameter
-                for i, name in enumerate(param.names):
-                    out_dict[name] = {'values':param_out[i],
-                                    'unit':param.units[i]}
-            else:
-                out_dict[param.full_name] = {'values':param_out,
-                                            'unit':param.unit}
-
-        return out_dict
 
     def snapshot_base(self, update=False):
         # TODO: Make sure the snapshot contains all the necessary information
